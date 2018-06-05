f67d04200f37a048023290e5ed44e555b10187e1
Bartosz-D3V <nix.noxi379@gmail.com>
2018-06-05 10:21:12 +0100

    fixed ci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CreditCardModule } from 'credit-card';
+
+import { CreditCardModule } from '@cc-project/lib/credit-card.module';
+// import { CreditCardModule } from 'credit-card';
 
 @NgModule({
   declarations: [AppComponent],
