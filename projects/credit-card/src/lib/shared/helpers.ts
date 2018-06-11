e974408fc19a11c4ff6b7b3d57706981096eac73
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-06-11 17:16:16 +0200

    tests(cc): add missing helpers unit tests (#18)

--- projects/credit-card/src/lib/shared/helpers.ts
+++ projects/credit-card/src/lib/shared/helpers.ts
@@ -1,2 +1,5 @@
-export const range = (start: number, end: number) =>
-  Array.from({ length: end - start }, (v: number, k: number) => k + start).map((m: number) => m.toString());
+export const range = (start: number, end: number): Array<number> =>
+  start != null && end != null ? Array.from({ length: end - start + 1 }, (v: number, k: number) => k + start) : null;
+
+export const stringify = <T>(array: Array<T>): Array<string> =>
+  array != null ? array.map((n: T) => n.toString()) : null;
