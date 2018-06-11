f2aeae03a57c40d298213212b5ed9ee1c41d466b
Bartosz-D3V <nix.noxi379@gmail.com>
2018-06-11 14:01:53 +0100

    fix lint issues

--- projects/credit-card/src/lib/shared/helpers.ts
+++ projects/credit-card/src/lib/shared/helpers.ts
@@ -1,2 +1,2 @@
-export const range = (start, end) =>
-  Array.from({ length: end - start }, (v, k) => k + start).map((m: number) => m.toString());
+export const range = (start: number, end: number) =>
+  Array.from({ length: end - start }, (v: number, k: number) => k + start).map((m: number) => m.toString());
