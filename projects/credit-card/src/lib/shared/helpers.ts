37240b8f52f322b69622d2a53954e982c76c1495
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-06-12 11:49:13 +0200

    refactor(cc): rename stringify function to stringifyArray (#19)

--- projects/credit-card/src/lib/shared/helpers.ts
+++ projects/credit-card/src/lib/shared/helpers.ts
@@ -1,5 +1,5 @@
 export const range = (start: number, end: number): Array<number> =>
   start != null && end != null ? Array.from({ length: end - start + 1 }, (v: number, k: number) => k + start) : null;
 
-export const stringify = <T>(array: Array<T>): Array<string> =>
+export const stringifyArray = <T>(array: Array<T>): Array<string> =>
   array != null ? array.map((n: T) => n.toString()) : null;
