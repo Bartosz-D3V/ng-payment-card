871498c4bd3f795bff0137021028a6ca7e52aa4c
Bartosz-D3V <nix.noxi379@gmail.com>
2018-06-27 13:55:57 +0200

    feat(cc): add input properties for text and validation

--- projects/credit-card/src/lib/credit-card.component.ts
+++ projects/credit-card/src/lib/credit-card.component.ts
@@ -6,6 +6,7 @@ import {
   OnChanges,
   SimpleChanges,
   ChangeDetectionStrategy,
+  Input,
 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -20,6 +21,48 @@ import { CardDetails } from '@cc-project/lib/domain/CardDetails';
 })
 export class CreditCardComponent implements OnInit {
   public ccForm: FormGroup;
+
+  @Input() public ccNumMissingTxt? = 'Card number is required';
+
+  @Input() public ccNumTooShortTxt? = 'Card number is too short';
+
+  @Input() public ccNumTooLongTxt? = 'Card number is too long';
+
+  @Input() public ccNumContainsLettersTxt? = 'Card number can contain digits only';
+
+  @Input() public ccNumchecksumInvalidTxt? = 'Provided card number is invalid';
+
+  @Input() public cardHolderMissingTxt? = 'Card holder name is required';
+
+  @Input() public expirationDayMissingTxt? = 'Expiration day is required';
+
+  @Input() public expirationDayTooShortTxt? = 'Expiration day is too short';
+
+  @Input() public expirationDayTooLongTxt? = 'Expiration day is too long';
+
+  @Input() public expirationMonthMissingTxt? = 'Expiration month is required';
+
+  @Input() public expirationMonthTooShortTxt? = 'Expiration month is too short';
+
+  @Input() public expirationMonthTooLongTxt? = 'Expiration month is too long';
+
+  @Input() public ccvMissingTxt? = 'CCV number is required';
+
+  @Input() public ccvNumTooShortTxt? = 'CCV number is too short';
+
+  @Input() public ccvNumTooLongTxt? = 'CCV number is too long';
+
+  @Input() public ccvContainsLettersTxt? = 'CCV number can contain digits only';
+
+  @Input() public validateCCNum? = true;
+
+  @Input() public validateCardHolder? = true;
+
+  @Input() public validateExpirationDay? = true;
+
+  @Input() public validateExpirationMonth? = true;
+
+  @Input() public validateCCV? = true;
 
   @Output() public formSaved: EventEmitter<ICardDetails> = new EventEmitter<CardDetails>();
 
