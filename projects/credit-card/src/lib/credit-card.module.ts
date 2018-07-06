5046a3c8553bc7fa7b61f8c34c6dae0971cd34cc
Bartosz-D3V <nix.noxi379@gmail.com>
2018-07-06 10:34:36 +0100

    docs: added some missing JSDOC comments

--- projects/credit-card/src/lib/credit-card.module.ts
+++ projects/credit-card/src/lib/credit-card.module.ts
@@ -3,6 +3,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { CreditCardComponent } from './credit-card.component';
 
+/**
+ * Monolithic module that is being bundled and published.
+ * Depends only on ReactiveFormsModule.
+ */
 @NgModule({
   imports: [ReactiveFormsModule],
   declarations: [CreditCardComponent],
