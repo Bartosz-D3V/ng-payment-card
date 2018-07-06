33cc2436f870f50d918cb8fe6a166d1916d5f20f
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-07-06 11:05:17 +0100

    docs: added some missing JSDOC comments (#40)

--- projects/credit-card/src/lib/credit-card.component.ts
+++ projects/credit-card/src/lib/credit-card.component.ts
@@ -7,60 +7,163 @@ import { CardDetails } from '@cc-project/lib/domain/CardDetails';
 import { CardType } from '@cc-project/lib/domain/card-type';
 import { default as CARD_TYPES, CardTypesContainer } from './domain/card-types';
 
+/**
+ * NgCreditCard without any dependencies other then ReactiveFormsModule
+ */
 @Component({
   selector: 'ng-credit-card',
   templateUrl: './credit-card.component.html',
   styleUrls: ['./credit-card.component.scss'],
 })
 export class CreditCardComponent implements OnInit {
+  /**
+   * FormGroup available publicly
+   */
   public ccForm: FormGroup;
 
+  /**
+   * Readonly card types container.
+   * Contains list of card types as enum and list of regexps
+   * @type {Map<CardType, RegExp>}
+   */
   public readonly cardTypes: CardTypesContainer = CARD_TYPES;
 
+  /**
+   * Validation message for missing credit card number
+   * @type {string} [ccNumMissingTxt] - Card number is required
+   */
   @Input() public ccNumMissingTxt? = 'Card number is required';
 
+  /**
+   * Validation message for too short credit card number
+   * @type {string} [ccNumTooShortTxt] - Card number is too short
+   */
   @Input() public ccNumTooShortTxt? = 'Card number is too short';
 
+  /**
+   * Validation message for too long credit card number
+   * @type {string} [ccNumTooLongTxt] - Card number is too long
+   */
   @Input() public ccNumTooLongTxt? = 'Card number is too long';
 
+  /**
+   * Validation message for credit card number that contains characters other than digits
+   * @type {string} [ccNumContainsLettersTxt] - Card number can contain digits only
+   */
   @Input() public ccNumContainsLettersTxt? = 'Card number can contain digits only';
 
+  /**
+   * Validation message for invalid credit card  number (Luhn's validation)
+   * @type {string} [ccNumChecksumInvalidTxt] - Provided card number is invalid
+   */
   @Input() public ccNumChecksumInvalidTxt? = 'Provided card number is invalid';
 
+  /**
+   * Validation message for missing card holder name
+   * @type {string} [cardHolderMissingTxt] - Card holder name is required
+   */
   @Input() public cardHolderMissingTxt? = 'Card holder name is required';
 
+  /**
+   * Validation message for missing expiration day
+   * @type {string} [expirationDayMissingTxt] - Expiration day is required
+   */
   @Input() public expirationDayMissingTxt? = 'Expiration day is required';
 
+  /**
+   * Validation message for too short expiration day
+   * @type {string} [expirationDayTooShortTxt] - Expiration day is too short
+   */
   @Input() public expirationDayTooShortTxt? = 'Expiration day is too short';
 
+  /**
+   * Validation message for too long expiration day
+   * @type {string} [expirationDayTooLongTxt] - Expiration day is too long
+   */
   @Input() public expirationDayTooLongTxt? = 'Expiration day is too long';
 
+  /**
+   * Validation message for missing expiration month
+   * @type {string} [expirationMonthMissingTxt] - Expiration month is required
+   */
   @Input() public expirationMonthMissingTxt? = 'Expiration month is required';
 
+  /**
+   * Validation message for too short expiration month
+   * @type {string} [expirationMonthTooShortTxt] - Expiration month is too short
+   */
   @Input() public expirationMonthTooShortTxt? = 'Expiration month is too short';
 
+  /**
+   * Validation message for too long expiration month
+   * @type {string} [expirationMonthTooLongTxt] - Expiration month is too long
+   */
   @Input() public expirationMonthTooLongTxt? = 'Expiration month is too long';
 
+  /**
+   * Validation message for missing CCV number
+   * @type {string} [ccvMissingTxt] - CCV number is required
+   */
   @Input() public ccvMissingTxt? = 'CCV number is required';
 
+  /**
+   * Validation message for too short CCV number
+   * @type {string} [ccvNumTooShortTxt] - CCV number is too short
+   */
   @Input() public ccvNumTooShortTxt? = 'CCV number is too short';
 
+  /**
+   * Validation message for too long CCV number
+   * @type {string} [ccvNumTooLongTxt] - CCV number is too long
+   */
   @Input() public ccvNumTooLongTxt? = 'CCV number is too long';
 
+  /**
+   * Validation message for incorrect CCV number containing characters other than digits
+   * @type {string} [ccvContainsLettersTxt] - CCV number can contain digits only
+   */
   @Input() public ccvContainsLettersTxt? = 'CCV number can contain digits only';
 
+  /**
+   * Switch validation of the credit card number
+   * @type {boolean} [validateCCNum] - true
+   */
   @Input() public validateCCNum? = true;
 
+  /**
+   * Switch validation of the credit card holder
+   * @type {boolean} [validateCardHolder] - true
+   */
   @Input() public validateCardHolder? = true;
 
+  /**
+   * Switch validation of the credit card expiration day
+   * @type {boolean} [validateExpirationDay] - true
+   */
   @Input() public validateExpirationDay? = true;
 
+  /**
+   * Switch validation of the credit card expiration month
+   * @type {boolean} [validateExpirationMonth] - true
+   */
   @Input() public validateExpirationMonth? = true;
 
+  /**
+   * Switch validation of the credit card CCV number
+   * @type {boolean} [validateCCV] - true
+   */
   @Input() public validateCCV? = true;
 
+  /**
+   * EventEmitter for credit card object
+   * @type {EventEmitter<CardDetails>}
+   */
   @Output() public formSaved: EventEmitter<ICardDetails> = new EventEmitter<CardDetails>();
 
+  /**
+   * Constructor for component injecting FormBuilder from ReactiveFormsModule
+   * @param {FormBuilder} _fb
+   */
   constructor(private _fb: FormBuilder) {}
 
   public ngOnInit(): void {
