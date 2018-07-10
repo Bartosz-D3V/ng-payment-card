01581c5b111770891e1fb98d33d368b235f996ae
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-07-10 15:27:01 +0100

    feat(cc): pipe for formatting credit card number (#45)

---
+++ projects/credit-card/src/lib/pipe/credit-card-number.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'creditCardNumber',
+})
+export class CreditCardNumberPipe implements PipeTransform {
+  public transform(value: string): string {
+    switch (value.length) {
+      case 15:
+        value = value.replace(/\b(\d{4})/, '$1-');
+        value = value.replace(/-(\d{6})/, '-$1-');
+        return value;
+      case 16:
+        return value.match(/.{4}/g).join('-');
+      default:
+        return value;
+    }
+  }
+}
