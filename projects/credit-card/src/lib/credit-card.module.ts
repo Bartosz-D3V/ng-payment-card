5ca4f1012941bb4cefcdd86c2d8da9dd8c7dbbf8
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-07-13 14:01:49 +0100

    refactored cc template (#49)

--- projects/credit-card/src/lib/credit-card.module.ts
+++ projects/credit-card/src/lib/credit-card.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { CreditCardComponent } from './credit-card.component';
 import { CreditCardNumberPipe } from './pipe/credit-card-number.pipe';
 
 @NgModule({
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   declarations: [CreditCardComponent, CreditCardNumberPipe],
   exports: [CreditCardComponent],
 })
