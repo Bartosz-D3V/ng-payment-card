026faa6bdf8977dd0cf46825b59a5f588dce8dcd
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-08-09 13:37:43 +0100

    feat(cc): logo display (#60)

--- projects/credit-card/src/lib/service/credit-card.service.ts
+++ projects/credit-card/src/lib/service/credit-card.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 
 import { default as CARD_TYPES, CardTypesContainer } from '@cc-project/lib/domain/card-types';
-import { CardType } from '@cc-project/lib/domain/card-type';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +8,7 @@ import { CardType } from '@cc-project/lib/domain/card-type';
 export class CreditCardService {
   private static readonly cardTypes: CardTypesContainer = CARD_TYPES;
 
-  public static getCardType(ccNum: string): CardType | null {
+  public static getCardType(ccNum: string): string | null {
     for (const [key, val] of Array.from(CreditCardService.cardTypes.entries())) {
       if (
         ccNum
