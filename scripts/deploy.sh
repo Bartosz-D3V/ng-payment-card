8f3104d647f1f6ae3d3439a3aa705c606ebbbe2f
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-08-13 16:10:12 +0100

    fix(sh): fix shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,4 +13,6 @@ git config user.email "github@travis-ci.org"
 git add . --force
 git commit -m "Deploy to GitHub Pages"
 
-git push --force "https://${GH_REF}" master:gh-pages
+echo 'Pushing to gh-pages...'
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+echo 'Done'
